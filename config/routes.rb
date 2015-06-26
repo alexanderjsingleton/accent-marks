ddd53b2f418710b5c7377325ca893deaf0f4d1e8
Alex Singleton <alexanderjsingleton@gmail.com>
2015-06-26 13:31:46 -0500

    stop

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
     root :to => "users#new"
   resources :users
   resources :sessions
-
-  # root to: 'welcome#index'
 end
